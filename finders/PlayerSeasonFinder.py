36910d1e36d1a363a6a15a8139de42fe452c5054
mdgoldberg <mgoldberg@college.harvard.edu>
2015-08-13 08:58:44 -0400

    work on PlayerSeasonFinder and constants

---
+++ finders/PlayerSeasonFinder.py
@@ -0,0 +1,83 @@
+from copy import deepcopy
+import json
+import os
+from pprint import pprint
+import time
+
+from constants import constants
+
+def PlayerSeasonFinder(**kwargs):
+
+    opts = kwArgsToOpts(**kwargs)
+    querystring = '&'.join(['{}={}'.format(k, v) for k, v in opts.iteritems()])
+    url = ('http://www.pro-football-reference.com/' +
+           'play-index/psl_finder.cgi?' +
+           querystring)
+    print url
+
+def kwArgsToOpts(**kwargs):
+
+    # if positions addressed in one kwarg, give it priority over pos_is_X
+    # entries by overwriting each of those it addresses
+    ext = {}
+    for k, v in kwargs.iteritems():
+        if k.lower() in ('pos', 'position', 'positions'):
+            v = [v] if not isinstance(v, list) else v
+            for pos in constants['POSITIONS']:
+                if pos in v:
+                    ext['pos_is_' + pos] = 'Y'
+    kwargs.update(ext)
+
+    # same as above for draft position
+    ext = {}
+    for k, v in kwargs.iteritems():
+        if k.lower() in ('draft_pos', 'draft_position', 'draft_positions'):
+            v = [v] if not isinstance(v, list) else v
+            for pos in constants['POSITIONS']:
+                if pos in v:
+                    ext['draft_pos_is_' + pos] = 'Y'
+                else:
+                    ext['draft_pos_is_' + pos] = 'N'
+    kwargs.update(ext)
+
+    # start with defaults
+    opts = deepcopy(constants['INPUTS_DEFAULTS'])
+    # update based on kwargs
+    for k, v in kwargs.iteritems():
+        # small changes to keys/values for convenience
+        k = 'team_id' if k in ('tm', 'team') else k
+        v = 'Y' if v == True else v
+        v = 'N' if v == False else v
+        # if overwriting a default
+        if k in opts:
+            opts[k] = v
+
+    # if no positions were selected, then select all positions
+    noPos = True
+    for k in kwargs:
+        if k.startswith('pos_is_') and kwargs[k] in ('Y', True, 'y'):
+            noPos = False
+            break
+    if noPos:
+        for pos in constants['POSITIONS']:
+            opts['pos_is_' + pos] = 'Y'
+
+    # same as above for draft positions
+    noPos = True
+    for k in kwargs:
+        if k.startswith('draft_pos_is_') and kwargs[k] in ('Y', True, 'y'):
+            noPos = False
+            break
+    if noPos:
+        for pos in constants['POSITIONS']:
+            opts['draft_pos_is_' + pos] = 'Y'
+
+    # turning on draft flag if necessary
+    draft = False
+    for k in opts:
+        if k in constants['DRAFT_INPUTS']:
+            draft = True
+    if draft:
+        opts['draft'] = '1'
+
+    return opts
--- finders/finders.py
+++
@@ -1,9 +0,0 @@
-import json
-
-
-
-def PlayerSeasonFinder(request=1, match='single', year_min=1920, year_max=2015,
-                       season_start=1, season_end=-1, age_min=0, age_max=99,
-                       league_id=None, team_id=None, is_active=None, is_hof=None,
-                       pos):
-
