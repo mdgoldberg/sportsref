b14dccb42bad5c12f503a3c11d2cb5c0071ccfec
mdgoldberg <mgoldberg@college.harvard.edu>
2015-08-18 03:06:44 -0400

    autoload PSF docstring in __init__.py

--- pfr/finders/__init__.py
+++ pfr/finders/__init__.py
@@ -1,3 +1,32 @@
 __all__ = [
     'PlayerSeasonFinder'
 ]
+
+# Fill in PlayerSeasonFinder docstring
+from pfr.finders.PlayerSeasonFinder import PlayerSeasonFinder
+from pfr.finders.PlayerSeasonFinder import getConstants
+
+constants = getConstants()
+inpDefs = constants['INPUTS_DEFAULTS']
+compStats = constants['COMP_STATS']
+sortStats = constants['SORT_STATS']
+
+paramDocstring = '\n'.join(':param {}: default="{}"'.format(k, v)
+                           for k, v in sorted(inpDefs.iteritems()))
+compStatsString = '\n'.join('* {}'.format(cs) for cs in compStats)
+sortStatsString = '\n'.join('* {}'.format(ss) for ss in sortStats)
+
+
+PlayerSeasonFinder.__doc__ = """
+Finds player-seasons that match criteria supplied by keyword arguments.
+
+{}
+:returns: list of matching player-season tuples
+:rtype: [(player relative URL, season year)]
+
+Options for comparison stats:
+{}
+
+Options for sorting stats:
+{}
+""".format(paramDocstring, compStatsString, sortStatsString)
