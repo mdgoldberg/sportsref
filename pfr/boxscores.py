10e183975e3ab6c8dadd77407a84d18bedfa492d
mdgoldberg <mgoldberg@college.harvard.edu>
2015-08-19 08:10:44 -0400

    compile date regex in boxscores

--- pfr/boxscores.py
+++ pfr/boxscores.py
@@ -5,6 +5,8 @@ from urlparse import urljoin as _urljoin
 
 from pfr.players import getGamelogURL as _getGamelogURL
 
+_dateRegex = _re.compile(r'^\d{4}\-\d{2}\-\d{2}$')
+
 def getBoxScoreURLs(playerURL, year):
     """Get list of box score URLs for a given player-season.
 
@@ -19,7 +21,7 @@ def getBoxScoreURLs(playerURL, year):
     bsURLs = [boxscore_a.get('href')
               for boxscore_a in 
               soup.select('table#stats a[href*="/boxscores/"]')
-              if _re.match(r'^\d{4}\-\d{2}\-\d{2}$', boxscore_a.string)
+              if _re.match(_dateRegex, boxscore_a.string)
               ]
     return bsURLs
 
