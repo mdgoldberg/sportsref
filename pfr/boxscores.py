48520dcf9c0458071df069ae42f9705c65d197da
mdgoldberg <mgoldberg@college.harvard.edu>
2015-08-19 06:35:10 -0400

    imports with underscores so they are ignored

--- pfr/boxscores.py
+++ pfr/boxscores.py
@@ -2,10 +2,10 @@ from bs4 import BeautifulSoup as _BeautifulSoup
 import requests as _requests
 from urlparse import urljoin as _urljoin
 
-from pfr.players import getGamelogURL
+from pfr.players import getGamelogURL as _getGamelogURL
 
 def PStoBoxScoreURLs(playerURL, year):
-    gamelogURL = getGamelogURL(playerURL, year)
+    gamelogURL = _getGamelogURL(playerURL, year)
     html = _requests.get(gamelogURL).text
     soup = _BeautifulSoup(html, 'lxml')
     bsURLs = [boxscore_a.get('href')
