e4a6e1a1a21a857359605e23d55e1191efc675fc
mdgoldberg <mgoldberg@college.harvard.edu>
2015-09-06 01:21:19 -0400

    minor tweaks to test_script

--- test_script.py
+++ test_script.py
@@ -4,13 +4,14 @@ import pandas as pd
 from pprint import pprint
 import sys
 
-from pfr.finders import PlayerSeasonFinder, GamePlayFinder
-from pfr.utils import relURLToPlayerID
+from pfr import utils
+from pfr.finders import PlayerSeasonFinder as PSF
+from pfr.finders import GamePlayFinder as GPF
 
 # RBs with >= 100 rushes since 2013
-psf = PlayerSeasonFinder(
+psf = PSF.PlayerSeasonFinder(
     pos='rb', year_min=2013, order_by='fantasy_points_per_game',
-    c1stat='rush_att', c1val=100, c1comp='gt'
+    c1stat='rush_att', c1val=100, c1comp='gt', verbose=True
 )
 
 print len(psf)
@@ -18,11 +19,11 @@ print len(psf)
 dfs = []
 pIDs = []
 for ps in psf:
-    pURL, yr = ps
-    pID = relURLToPlayerID(pURL)
+    pID, yr = ps
     pIDs.append(pID)
-    df = GamePlayFinder(player_id=pID, year_min=yr, year_max=yr, type='rush')
+    df = GPF.GamePlayFinder(player_id=pID, year=yr, type='rush')
+    df['playerID'] = pID
     dfs.append(df)
 
-total_df = pd.concat(dfs, keys=pIDs)
-total_df.to_csv('total_df.csv')
+total_df = pd.concat(dfs)
+total_df.to_csv('total_df.csv', index=False)
