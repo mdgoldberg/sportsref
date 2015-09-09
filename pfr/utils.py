d110a5c91038a3ec44f34d857a9056765bf73b2d
mdgoldberg <mgoldberg@college.harvard.edu>
2015-09-09 00:30:49 -0400

    only import modules, change to relURLToID

--- pfr/utils.py
+++ pfr/utils.py
@@ -1,10 +1,13 @@
 import os
-import requests
+import re
 import time
+
+import numpy as np
+import requests
 
 __all__ = [
     'getHTML',
-    'relURLToPlayerID',
+    'relURLToID',
     'parsePlayDetails',
 ]
 
@@ -44,27 +47,92 @@ def getHTML(url):
     time.sleep(2)
     return html
 
-def relURLToPlayerID(url):
-    """Converts relative or absolute player URL to player ID.
+def relURLToID(url):
+    """Converts relative PFR URL to ID.
 
-    :returns: playerID associated with the given player URL.
+    Here, 'ID' refers generally to the unique ID for a given 'type' that a
+    given datum has. For example, 'BradTo00' is Tom Brady's player ID - this
+    corresponds to his relative URL, '/players/B/BradTo00.htm'. Similarly,
+    '201409070dal' refers to the boxscore of the SF @ DAL game on 09/07/14.
+
+    Supported types:
+    * player/...
+    * boxscores/...
+
+    :returns: ID associated with the given relative URL.
     """
-    playerID, _ = os.path.splitext(os.path.basename(url))
-    return playerID
+    playerRegex = re.compile(r'/players/[A-Z]/(\w+)\.html?', re.IGNORECASE)
+    boxscoresRegex = re.compile(r'/boxscores/(\w+)\.html?', re.IGNORECASE)
+
+    # check if player ID
+    match = playerRegex.match(url)
+    if match:
+        return match.group(1)
+    
+    # check if boxscores ID
+    match = boxscoresRegex.match(url)
+    if match:
+        return match.group(1)
+
+    return None
 
 def parsePlayDetails(details):
+    """Parses play details from play-by-play and returns structured data.
+    
+    Currently only handles passes and rushes.
+
+    :returns: dictionary of play attributes
     """
     
-    :returns:
-    """
-    details = details.lower()
-    
-    ptype = 'pass' if 'pass' in details else 'rush'
+    RUSH_OPTS = {
+        'left end': 'LE', 'left tackle': 'LT', 'left guard': 'LG',
+        'up the middle': 'M',
+        'right end': 'RE', 'right tackle': 'RT', 'right guard': 'RG',
+        '': np.nan
+    }
+    PASS_OPTS = {
+        'short left': 'SL', 'short middle': 'SM', 'short right': 'SR',
+        'deep left': 'DL', 'deep middle': 'DM', 'deep right': 'DR',
+        '': np.nan
+    }
+
+    # have to sort them to make sure it matches empty string last
+    rushOptRE = r'(?P<rushDir>{})'.format(
+        r'|'.join(sorted(RUSH_OPTS.iterkeys(), reverse=True))
+    )
+    passOptRE = r'(?P<passLoc>{})'.format(
+        r'|'.join(sorted(PASS_OPTS.iterkeys(), reverse=True))
+    )
+
+    playerRE = r"(?P<rusher>\S{6}\d{2})"
+    rushOptRE = r"(?: {})?".format(rushOptRE)
+    yardsRE = r"(?:(?:(?P<yds>\-?\d+) yards?)|(?:no gain))"
+    # cases after this: tackle, fumble, or nothing
+
+    rushREstr = (
+        r"{}{} for {}"
+    ).format(playerRE, rushOptRE, yardsRE)
+    print rushREstr
+    rushRE = re.compile(rushREstr, re.IGNORECASE)
+    return rushRE
+    match = rushRE.match(details)
+    return match
+
+
+    # first, figure out play type
+    if ' pass ' in details or ' sacked ' in details:
+        ptype = 'pass'
+    # below line not tested
+    elif any([ro in details for ro in RUSH_OPTS.itervalues()]):
+        ptype = 'rush'
+
     if ptype == 'pass':
+        # analyze the pass
         pass
 
-    RUSH_OPTS = ['LE', 'LT', 'LG', 'M', 'RG', 'RT', 'RE']
-    PASS_OPTS = ['SL', 'SM', 'SR', 'DL', 'DM', 'DR']
+    elif ptype == 'rush':
+        # analyze the rush
+        pass
 
 
     return None
