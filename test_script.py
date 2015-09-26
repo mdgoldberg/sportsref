48aa2c3305980c1f80f1917db9847ad4eccefcd7
mdgoldberg <mgoldberg@college.harvard.edu>
2015-09-26 18:08:59 -0400

    small test_script changes

--- test_script.py
+++ test_script.py
@@ -8,21 +8,21 @@ from pfr import utils
 from pfr.finders import PlayerSeasonFinder as PSF
 from pfr.finders import GamePlayFinder as GPF
 
-# RBs with >= 100 rushes since 2013
+# RBs with >= 100 rushes since 2010
 psf = PSF.PlayerSeasonFinder(
-    pos='rb', year_min=2013, order_by='fantasy_points_per_game',
+    pos='rb', year_min=2010, order_by='rush_att',
     c1stat='rush_att', c1val=100, c1comp='gt', verbose=True
 )
 
 print len(psf)
 
 dfs = []
-pIDs = []
+pURLs = []
 for ps in psf:
-    pID, yr = ps
-    pIDs.append(pID)
-    df = GPF.GamePlayFinder(player_id=pID, year=yr, type='rush')
-    df['playerID'] = pID
+    print ps
+    pURL, yr = ps
+    pURLs.append(pURL)
+    df = GPF.GamePlayFinder(player_id=pURL, year=yr, type='rush')
     dfs.append(df)
 
 total_df = pd.concat(dfs)
