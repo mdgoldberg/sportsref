eadc897f3c75befa1462cceb8d1cdff07ae60115
mdgoldberg <mgoldberg@college.harvard.edu>
2015-10-11 00:52:21 -0400

    update to playtest script

--- playTest.py
+++ playTest.py
@@ -5,10 +5,7 @@ import pandas as pd
 from pfr.finders import GamePlayFinder as GPF
 from pfr import utils
 
-r = GPF.GamePlayFinder(playerID='BellLe00', type='RUSH', yr=2014, yards=3, yg_gtlt='lt')
-r2 = GPF.GamePlayFinder(yr=2014, type='RUSH', yards=-2, yg_gtlt='lt')
-r = pd.concat([r,r2])
-del r2
+r = GPF.GamePlayFinder(type='RUSH', yr=2014)
 
 matches = r.Detail.apply(lambda x: (x, utils.parsePlayDetails(x)))
 missed = filter(lambda x: not x[1], matches)
