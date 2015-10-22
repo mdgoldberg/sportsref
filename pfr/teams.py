795cfc0c01e29217bf056b61b606cdde2ee67ae1
mdgoldberg <mgoldberg@college.harvard.edu>
2015-10-22 00:45:15 -0400

    boxscores function

--- pfr/teams.py
+++ pfr/teams.py
@@ -1,12 +1,13 @@
 import datetime
 import re
+import urlparse
 
 import requests
 import numpy as np
 import pandas as pd
 from pyquery import PyQuery as pq
 
-from pfr import players, utils, BASE_URL
+from pfr import boxscores, players, utils, BASE_URL
 
 __all__ = [
     'listTeams',
@@ -25,12 +26,28 @@ class Team:
 
     def __init__(self, teamID):
         self.teamID = teamID
+        self.teamURL = urlparse.urljoin(
+            BASE_URL, '/teams/{}'.format(self.teamID))
+        self.teamYearURL = lambda yr: urlparse.urljoin(
+            BASE_URL, '/teams/{}/{}.htm'.format(self.teamID, yr))
 
     def roster(self, year=yr):
         """Returns the roster table for the given year.
 
-        :year: The year for which we want the roster.
+        :year: The year for which we want the roster; defaults to current year.
         :returns: A DataFrame containing roster information for that year.
-
         """
         raise "not yet implemented"
+
+    def boxscores(self, year=yr):
+        """Gets list of BoxScore objects corresponding to the box scores from
+        that year.
+
+        :year: The year for which we want the boxscores; defaults to current
+        year.
+        :returns: list of BoxScore objects
+        """
+        doc = pq(self.teamYearURL(year))
+        table = doc('table#team_gamelogs')
+        df = utils.parseTable(table)
+        return df.boxscore_word.apply(boxscores.BoxScore).values
