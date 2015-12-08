07e5e9459f2b1545b889de01dc8cb2c385966adb
mdgoldberg <mgoldberg@college.harvard.edu>
2015-12-07 23:37:18 -0500

    release 0.4.10

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='pfr',
-      version='0.4.9',
+      version='0.4.10',
       description='Scraping data from pro-football-reference.com',
       url='https://github.com/mdgoldberg/pfr',
       author='Matt Goldberg',
