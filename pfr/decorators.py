420ae5efe084aa004cbb8741c29f038d50f726f6
mdgoldberg <mgoldberg@college.harvard.edu>
2016-01-10 03:12:09 -0500

    smart cache updating based on whether it is in-season or offseason and possible gametimes

--- pfr/decorators.py
+++ pfr/decorators.py
@@ -1,4 +1,5 @@
 import collections
+import datetime
 import functools
 import os
 import time
@@ -39,20 +40,37 @@ def cacheHTML(func):
         relURL = parsed.path
         if parsed.query:
             relURL += '?' + parsed.query
-        relURL = relURL.strip('/').replace('/', '__')
-        fn = '{}/{}'.format(CACHE_DIR, relURL)
+        noPathFN = relURL.replace('/', '')
+        fn = '{}/{}'.format(CACHE_DIR, noPathFN)
 
         # TODO: fix this problem?
-        if len(fn) > 255:
+        if len(noPathFN) > 255:
             # filename is too long, just evaluate the function
-            return func(url)#.encode('ascii', 'replace')
+            return func(url).encode('ascii', 'replace')
         
-        # set time variables
+        # set time variables (in seconds)
         if os.path.isfile(fn):
             modtime = int(os.path.getmtime(fn))
             curtime = int(time.time())
+
+        def cacheValid(ct, mt):
+            today = datetime.date.today()
+            endOfSeason = datetime.date(today.year, 2, 10)
+            startOfSeason = datetime.date(today.year, 9, 1)
+            # if we're in the offseason, don't worry about it
+            if endOfSeason < today < startOfSeason:
+                return True
+
+            # otherwise, check if new data could have been updated
+            # (assumed that new game data is added the day after that game)
+            modDay = today - datetime.timedelta(seconds=ct-mt)
+            lastGameDay = today
+            while (lastGameDay.weekday() + 1) % 7 not in (0, 1, 2, 5):
+                lastGameDay = lastGameDay - datetime.timedelta(days=1)
+            return modDay >= lastGameDay
+
         # if file found and it's been <= a month, read from file
-        if os.path.isfile(fn) and curtime - modtime <= 30*24*60*60:
+        if os.path.isfile(fn) and cacheValid(curtime, modtime):
             with open(fn, 'r') as f:
                 text = f.read()
             return text
@@ -69,6 +87,7 @@ def memoized(fun):
     """A simple memoize decorator."""
     @functools.wraps(fun)
     def wrapper(*args, **kwargs):
+        # TODO: deal with dicts in args
         key = (args, frozenset(sorted(kwargs.items())))
         try:
             ret = cache[key]
@@ -77,6 +96,7 @@ def memoized(fun):
             ret = cache[key] = fun(*args, **kwargs)
             return ret
         except TypeError:
+            print 'memoization type error here', fun.__name__, key
             return fun(*args, **kwargs)
 
     cache = {}
