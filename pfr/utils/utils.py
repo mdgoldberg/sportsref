eff27c5a500e8128fe4428cd815c27ec052b37af
mdgoldberg <mgoldberg@college.harvard.edu>
2016-01-31 00:44:21 -0500

    added PFR winProb functions

--- pfr/utils/utils.py
+++ pfr/utils/utils.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 from pyquery import PyQuery as pq
 import requests
+from scipy.stats import norm
 
 import pfr
 
@@ -184,3 +185,30 @@ def flattenLinks(td):
         return None
 
     return ''.join(_flattenC(c) for c in td.contents())
+
+def initialWinProb(line):
+    """Gets the initial win probability of a game given its Vegas line.
+
+    :line: The Vegas line from the home team's perspective (negative means
+    home team is favored).
+    :returns: A float in [0., 100.] that represents the win probability.
+    """
+    line = float(line)
+    probWin = 1. - norm.cdf(0.5, -line, 13.86)
+    probTie = norm.cdf(0.5, -line, 13.86) - norm.cdf(-0.5, -line, 13.86)
+    return 100. * (probWin + 0.5 * probTie)
+
+def winProb(line, margin, secsElapsed, expPts):
+    line = float(line)
+    margin = float(margin)
+    expPts = float(expPts)
+    baseMean = -line
+    baseStd = 13.46
+    expMargin = margin + expPts
+    minRemain = 60. - secsElapsed / 60. + 0.00001
+    adjMean = baseMean * (minRemain / 60.)
+    adjStd = baseStd / np.sqrt(60. / minRemain)
+    probWin = 1. - norm.cdf(-expMargin + 0.5, adjMean, adjStd)
+    probTie = (norm.cdf(-expMargin + 0.5, adjMean, adjStd) -
+               norm.cdf(-expMargin - 0.5, adjMean, adjStd))
+    return 100. * (probWin + 0.5*probTie)
