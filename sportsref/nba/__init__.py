c34be73137c7021aa29585d05898c1e7f2cdc783
mdgoldberg <mgoldberg@college.harvard.edu>
2016-03-13 20:28:14 -0400

    started NBA module for basketball-reference

---
+++ sportsref/nba/__init__.py
@@ -0,0 +1,10 @@
+BASE_URL = 'http://www.basketball-reference.com'
+
+import boxscores
+
+from boxscores import BoxScore
+
+__all__ = [
+    'BASE_URL',
+    'boxscores', 'BoxScore',
+]
---
+++ sportsref/nba/boxscores.py
@@ -0,0 +1,111 @@
+import datetime
+import re
+import urlparse
+
+from pyquery import PyQuery as pq
+
+import sportsref
+
+@sportsref.decorators.memoized
+class BoxScore:
+
+    def __init__(self, bsID):
+        self.bsID = bsID
+
+    def __eq__(self, other):
+        return self.bsID == other.bsID
+
+    def __hash__(self):
+        return hash(self.bsID)
+
+    @sportsref.decorators.memoized
+    def getMainDoc(self):
+        url = urlparse.urljoin(
+            sportsref.nba.BASE_URL, 'boxscores/{}.html'.format(self.bsID)
+        )
+        doc = pq(sportsref.utils.getHTML(url))
+        return doc
+
+    @sportsref.decorators.memoized
+    def getPBPDoc(self):
+        url = urlparse.urljoin(
+            sportsref.nba.BASE_URL, 'boxscores/pbp/{}.html'.format(self.bsID)
+        )
+        doc = pq(sportsref.utils.getHTML(url))
+        return doc
+    
+    @sportsref.decorators.memoized
+    def date(self):
+        """Returns the date of the game. See Python datetime.date documentation
+        for more.
+        :returns: A datetime.date object with year, month, and day attributes.
+        """
+        match = re.match(r'(\d{4})(\d{2})(\d{2})', self.bsID)
+        year, month, day = map(int, match.groups())
+        return datetime.date(year=year, month=month, day=day)
+
+    @sportsref.decorators.memoized
+    def weekday(self):
+        days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday',
+                'Saturday', 'Sunday']
+        date = self.date()
+        wd = date.weekday()
+        return days[wd]
+
+    @sportsref.decorators.memoized
+    def home(self):
+        """Returns home team ID.
+        :returns: 3-character string representing home team's ID.
+        """
+        raise 'not yet implemented'
+
+    @sportsref.decorators.memoized
+    def away(self):
+        """Returns away team ID.
+        :returns: 3-character string representing away team's ID.
+        """
+        raise 'not yet implemented'
+
+    @sportsref.decorators.memoized
+    def homeScore(self):
+        """Returns score of the home team.
+        :returns: int of the home score.
+        """
+        raise 'not yet implemented'
+
+    @sportsref.decorators.memoized
+    def awayScore(self):
+        """Returns score of the away team.
+        :returns: int of the away score.
+        """
+        raise 'not yet implemented'
+
+    @sportsref.decorators.memoized
+    def winner(self):
+        """Returns the team ID of the winning team. Returns NaN if a tie."""
+        hmScore = self.homeScore()
+        awScore = self.awayScore()
+        if hmScore > awScore:
+            return self.home()
+        elif hmScore < awScore:
+            return self.away()
+        else:
+            return np.nan
+
+    @sportsref.decorators.memoized
+    def season(self):
+        """
+        Returns the year ID of the season in which this game took place.
+        Useful for week 17 January games.
+
+        :returns: An int representing the year of the season.
+        """
+        raise 'not yet implemented'
+    
+    @sportsref.decorators.memoized
+    def pbp(self):
+        """Returns a dataframe of the play-by-play data from the game.
+
+        :returns: pandas DataFrame of play-by-play. Similar to GPF.
+        """
+        raise 'not yet implemented'
