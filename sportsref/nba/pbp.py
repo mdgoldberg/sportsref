d6035d7c9be86e60683fd1f3fd9bc2619124ee6a
mdgoldberg <mgoldberg@college.harvard.edu>
2017-01-10 23:03:16 -0500

    changed camelCase to snake_case in nba.pbp module

--- sportsref/nba/pbp.py
+++ sportsref/nba/pbp.py
@@ -6,292 +6,321 @@ import pandas as pd
 
 import sportsref
 
+player_re = r'\w{0,7}\d{2}'
+
 
 @sportsref.decorators.memoize
 def parse_play(details, hm, aw, is_hm, yr):
-    """Parse play details from a play-by-play string describing a play; returns
-    structured data in a dictionary.
+    """Parse play details from a play-by-play string describing a play.
 
-    :details: detail string for the play
-    :hm: the ID of the home team (for identifying team w/ possession)
-    :aw: the ID of the away team (for identifying team w/ possession)
-    :is_hm: bool indicating whether it is a home play (for possession)
-    :yr: year of the game
-    :returns: dictionary of play attributes; -1 on a play that should be
-    skipped
+    Assuming valid input, this function returns structured data in a dictionary
+    describing the play. If the play detail string was invalid, this function
+    returns None.
+
+    :param details: detail string for the play
+    :param hm: the ID of the home team
+    :param aw: the ID of the away team
+    :param is_hm: bool indicating whether the offense is at home
+    :param yr: year of the game
+    :param returns: dictionary of play attributes or None if invalid
+    :rtype: dictionary or None
     """
-    # if input isn't a string, return -1
-    if not isinstance(details, basestring):
-        return -1
+    # if input isn't a string, return None
+    if not isinstance(details, basestring) or not details:
+        return None
 
     p = {}
     p['detail'] = details
     p['home'] = hm
     p['away'] = aw
-    p['isHomePlay'] = is_hm
-
-    playerRE = r'\w{0,7}\d{2}'
+    p['is_home_play'] = is_hm
 
     # parsing field goal attempts
-    shotRE = (r'(?P<shooter>{0}) (?P<isFGM>makes|misses) (?P<isThree>2|3)\-pt'
-              r' shot').format(playerRE)
-    distRE = r' (?:from (?P<shotDist>\d+) ft|at rim)'
-    assistRE = r' \(assist by (?P<assister>{0})\)'.format(playerRE)
-    blockRE = r' \(block by (?P<blocker>{0})\)'.format(playerRE)
+    shotRE = (r'(?P<shooter>{0}) (?P<is_fgm>makes|misses) '
+              '(?P<is_three>2|3)\-pt shot').format(player_re)
+    distRE = r' (?:from (?P<shot_dist>\d+) ft|at rim)'
+    assistRE = r' \(assist by (?P<assister>{0})\)'.format(player_re)
+    blockRE = r' \(block by (?P<blocker>{0})\)'.format(player_re)
     shotRE = r'{0}{1}(?:{2}|{3})?'.format(shotRE, distRE, assistRE, blockRE)
     m = re.match(shotRE, details, re.IGNORECASE)
     if m:
-        p['isFGA'] = True
+        p['is_fga'] = True
         p.update(m.groupdict())
-        p['shotDist'] = p['shotDist'] if p['shotDist'] is not None else 0
-        p['shotDist'] = int(p['shotDist'])
-        p['isFGM'] = p['isFGM'] == 'makes'
-        p['isThree'] = p['isThree'] == '3'
-        p['isAssist'] = pd.notnull(p.get('assister'))
-        p['isBlock'] = pd.notnull(p.get('blocker'))
+        p['shot_dist'] = p['shot_dist'] if p['shot_dist'] is not None else 0
+        p['shot_dist'] = int(p['shot_dist'])
+        p['is_fgm'] = p['is_fgm'] == 'makes'
+        p['is_three'] = p['is_three'] == '3'
+        p['is_assist'] = pd.notnull(p.get('assister'))
+        p['is_block'] = pd.notnull(p.get('blocker'))
         p['team'] = hm if is_hm else aw
         p['opp'] = aw if is_hm else hm
         return p
 
     # parsing jump balls
-    jumpRE = (r'Jump ball: (?P<awayJumper>{0}) vs\. (?P<homeJumper>{0})'
-              r'(?: \((?P<gainsPos>{0}) gains possession\))?').format(playerRE)
+    jumpRE = ((r'Jump ball: (?P<away_jumper>{0}) vs\. (?P<home_jumper>{0})'
+               r'(?: \((?P<gains_poss>{0}) gains possession\))?')
+              .format(player_re))
     m = re.match(jumpRE, details, re.IGNORECASE)
     if m:
-        p['isJumpBall'] = True
+        p['is_jump_ball'] = True
         p.update(m.groupdict())
         return p
 
     # parsing rebounds
-    rebRE = (r'(?P<isOReb>Offensive|Defensive) rebound'
-             r' by (?P<rebounder>{0}|Team)').format(playerRE)
+    rebRE = (r'(?P<is_oreb>Offensive|Defensive) rebound'
+             r' by (?P<rebounder>{0}|Team)').format(player_re)
     m = re.match(rebRE, details, re.I)
     if m:
-        p['isReb'] = True
+        p['is_reb'] = True
         p.update(m.groupdict())
-        p['isOReb'] = p['isOReb'] == 'Offensive'
-        p['isDReb'] = not p['isOReb']
-        p['rebTeam'], other = (hm, aw) if is_hm else (aw, hm)
-        p['team'] = p['rebTeam'] if p['isOReb'] else other
-        p['opp'] = p['rebTeam'] if p['isDReb'] else other
+        p['is_oreb'] = p['is_oreb'].lower() == 'offensive'
+        p['is_dreb'] = not p['is_oreb']
+        p['reb_team'], other = (hm, aw) if is_hm else (aw, hm)
+        p['team'] = p['reb_team'] if p['is_oreb'] else other
+        p['opp'] = p['reb_team'] if p['is_dreb'] else other
         return p
 
     # parsing shooting fouls
-    shotFoulRE = (r'Shooting(?P<isBlockFoul> block)? foul by (?P<fouler>{0})'
-                  r'(?: \(drawn by (?P<drewFoul>{0})\))?').format(playerRE)
+    shotFoulRE = (r'Shooting(?P<is_block_foul> block)? foul by (?P<fouler>{0})'
+                  r'(?: \(drawn by (?P<drew_foul>{0})\))?').format(player_re)
     m = re.match(shotFoulRE, details, re.I)
     if m:
-        p['isShotFoul'] = True
+        p['is_foul'] = True
+        p['is_shot_foul'] = True
         p.update(m.groupdict())
-        p['isBlockFoul'] = p['isBlockFoul'] == ' block'
+        p['is_block_foul'] = bool(p['is_block_foul'])
         p['team'] = hm if is_hm else aw
         p['opp'] = aw if is_hm else hm
+        p['foul_team'] = p['opp']
         return p
 
     # parsing free throws
-    ftRE = (r'(?P<ftShooter>{}) (?P<isFTM>makes|misses) '
-            r'(?P<isTechFT>technical )?(?P<isFlagFT>flagrant )?'
-            r'(?P<isClearPathFT>clear path )?free throw'
-            r'(?: (?P<ftNum>\d+) of (?P<totFTAtt>\d+))?').format(playerRE)
+    ftRE = (r'(?P<ft_shooter>{}) (?P<is_ftm>makes|misses) '
+            r'(?P<is_tech_ft>technical )?(?P<is_flag_ft>flagrant )?'
+            r'(?P<is_clearpath_ft>clear path )?free throw'
+            r'(?: (?P<ft_num>\d+) of (?P<tot_fta>\d+))?').format(player_re)
     m = re.match(ftRE, details, re.I)
     if m:
-        p['isFTA'] = True
+        p['is_fta'] = True
         p.update(m.groupdict())
-        p['isFTM'] = p['isFTM'] == 'makes'
-        p['isTechFT'] = p['isTechFT'] == 'technical '
-        p['isFlagFT'] = p['isFlagFT'] == 'flagrant '
-        p['isClearPathFT'] = p['isClearPathFT'] == 'clear path '
-        if p['totFTAtt']:
-            p['totFTAtt'] = int(p['totFTAtt'])
-        if p['ftNum']:
-            p['ftNum'] = int(p['ftNum'])
+        p['is_ftm'] = p['is_ftm'] == 'makes'
+        p['is_tech_ft'] = bool(p['is_tech_ft'])
+        p['is_flag_ft'] = bool(p['is_flag_ft'])
+        p['is_clearpath_ft'] = bool(p['is_clearpath_ft'])
+        if p['tot_fta']:
+            p['tot_fta'] = int(p['tot_fta'])
+        if p['ft_num']:
+            p['ft_num'] = int(p['ft_num'])
         p['team'] = hm if is_hm else aw
         p['opp'] = aw if is_hm else hm
         return p
 
     # parsing substitutions
-    subRE = (r'(?P<subIn>{0}) enters the game for '
-             r'(?P<subOut>{0})').format(playerRE)
+    subRE = (r'(?P<sub_in>{0}) enters the game for '
+             r'(?P<sub_out>{0})').format(player_re)
     m = re.match(subRE, details, re.I)
     if m:
-        p['isSub'] = True
+        p['is_sub'] = True
         p.update(m.groupdict())
-        p['subTeam'] = hm if is_hm else aw
+        p['sub_team'] = hm if is_hm else aw
         return p
 
     # parsing turnovers
-    toReasons = (r'(?P<toType>[^;]+)(?:; steal by '
-                 r'(?P<stealer>{0}))?').format(playerRE)
-    toRE = (r'Turnover by (?P<turnoverBy>{}|Team) '
-            r'\((?:{})\)').format(playerRE, toReasons)
+    toReasons = (r'(?P<to_type>[^;]+)(?:; steal by '
+                 r'(?P<stealer>{0}))?').format(player_re)
+    toRE = (r'Turnover by (?P<to_by>{}|Team) '
+            r'\((?:{})\)').format(player_re, toReasons)
     m = re.match(toRE, details, re.I)
     if m:
-        p['isTO'] = True
+        p['is_to'] = True
         p.update(m.groupdict())
-        if p['toType'].lower() == 'offensive foul':
-            return -1
-        p['isSteal'] = pd.notnull(p['stealer'])
-        p['isTravel'] = p['toType'] == 'traveling'
-        p['isShotClockViol'] = p['toType'] == 'shot clock'
-        p['isOOB'] = p['toType'] == 'step out of bounds'
-        p['isThreeSecViol'] = p['toType'] == '3 sec'
-        p['isBackCourtViol'] = p['toType'] == 'back court'
-        p['isOffGoaltend'] = p['toType'] == 'offensive goaltending'
-        p['isDoubleDribble'] = p['toType'] == 'dbl dribble'
-        p['isDiscontDribble'] = p['toType'] == 'discontinued dribble'
-        p['isCarry'] = p['toType'] == 'palming'
+        p['to_type'] = p['to_type'].lower()
+        if p['to_type'] == 'offensive foul':
+            return None
+        p['is_steal'] = pd.notnull(p['stealer'])
+        p['is_travel'] = p['to_type'] == 'traveling'
+        p['is_shot_clock_viol'] = p['to_type'] == 'shot clock'
+        p['is_oob'] = p['to_type'] == 'step out of bounds'
+        p['is_three_sec_viol'] = p['to_type'] == '3 sec'
+        p['is_backcourt_viol'] = p['to_type'] == 'back court'
+        p['is_off_goaltend'] = p['to_type'] == 'offensive goaltending'
+        p['is_double_dribble'] = p['to_type'] == 'dbl dribble'
+        p['is_discont_dribble'] = p['to_type'] == 'discontinued dribble'
+        p['is_carry'] = p['to_type'] == 'palming'
         p['team'] = hm if is_hm else aw
         p['opp'] = aw if is_hm else hm
         return p
 
     # parsing offensive fouls
-    offFoulRE = (r'Offensive(?P<isCharge> charge)? foul '
-                 r'by (?P<turnoverBy>{0})'
-                 r'(?: \(drawn by (?P<drewFoul>{0})\))?').format(playerRE)
+    offFoulRE = (r'Offensive(?P<is_charge> charge)? foul '
+                 r'by (?P<to_by>{0})'
+                 r'(?: \(drawn by (?P<drew_foul>{0})\))?').format(player_re)
     m = re.match(offFoulRE, details, re.I)
     if m:
-        p['isFoul'] = True
-        p['isOffFoul'] = True
-        p['isTO'] = True
+        p['is_foul'] = True
+        p['is_off_foul'] = True
+        p['is_to'] = True
         p.update(m.groupdict())
-        p['isCharge'] = p['isCharge'] == ' charge'
-        p['fouler'] = p['turnoverBy']
+        p['is_charge'] = bool(p['is_charge'])
+        p['fouler'] = p['to_by']
         p['team'] = hm if is_hm else aw
         p['opp'] = aw if is_hm else hm
-        p['foulTeam'] = p['team']
+        p['foul_team'] = p['team']
         return p
 
     # parsing personal fouls
-    foulRE = (r'Personal (?P<isTakeFoul>take )?(?P<isBlockFoul>block )?foul by'
-              r' (?P<fouler>{0})(?: \(drawn by '
-              r'(?P<drewFoul>{0})\))?').format(playerRE)
+    foulRE = (r'Personal (?P<is_take_foul>take )?(?P<is_block_foul>block )?'
+              r'foul by (?P<fouler>{0})(?: \(drawn by '
+              r'(?P<drew_foul>{0})\))?').format(player_re)
     m = re.match(foulRE, details, re.I)
     if m:
-        p['isFoul'] = True
+        p['is_foul'] = True
         p.update(m.groupdict())
-        p['isTakeFoul'] = p['isTakeFoul'] == 'take '
-        p['isBlockFoul'] = p['isBlockFoul'] == 'block '
+        p['is_take_foul'] = bool(p['is_take_foul'])
+        p['is_block_foul'] = bool(p['is_block_foul'])
         p['team'] = aw if is_hm else hm
         p['opp'] = hm if is_hm else aw
-        p['foulTeam'] = p['opp']
+        p['foul_team'] = p['opp']
         return p
+
+    # TODO: parsing double personal fouls
+    # double_foul_re = (r'Double personal foul by (?P<fouler1>{0}) and '
+    #                   r'(?P<fouler2>{0})').format(player_re)
+    # m = re.match(double_Foul_re, details, re.I)
+    # if m:
+    #     p['is_foul'] = True
+    #     p.update(m.groupdict())
+    #     p['team'] =
 
     # parsing loose ball fouls
     looseBallRE = (r'Loose ball foul by (?P<fouler>{0})'
-                   r'(?: \(drawn by (?P<drewFoul>{0})\))?').format(playerRE)
+                   r'(?: \(drawn by (?P<drew_foul>{0})\))?').format(player_re)
     m = re.match(looseBallRE, details, re.I)
     if m:
-        p['isFoul'] = True
-        p['isLooseBallFoul'] = True
+        p['is_foul'] = True
+        p['is_loose_ball_foul'] = True
         p.update(m.groupdict())
-        p['foulTeam'] = hm if is_hm else aw
+        p['foul_team'] = hm if is_hm else aw
         return p
 
+    # parsing punching fouls
+    # TODO
+
     # parsing away from play fouls
-    awayFromBallRE = (r'Away from play foul by (?P<fouler>{0})'
-                      r'(?: \(drawn by (?P<drewFoul>{0})\))?').format(playerRE)
+    awayFromBallRE = ((r'Away from play foul by (?P<fouler>{0})'
+                       r'(?: \(drawn by (?P<drew_foul>{0})\))?')
+                      .format(player_re))
     m = re.match(awayFromBallRE, details, re.I)
     if m:
-        p['isFoul'] = True
-        p['isAwayFromBallFoul'] = True
+        p['is_foul'] = True
+        p['is_away_from_ball_foul'] = True
         p.update(m.groupdict())
         p['team'] = aw if is_hm else hm
         p['opp'] = hm if is_hm else aw
-        p['foulTeam'] = p['opp']
+        p['foul_team'] = p['opp']
         return p
 
     # parsing inbound fouls
     inboundRE = (r'Inbound foul by (?P<fouler>{0})'
-                 r'(?: \(drawn by (?P<drewFoul>{0})\))?').format(playerRE)
+                 r'(?: \(drawn by (?P<drew_foul>{0})\))?').format(player_re)
     m = re.match(inboundRE, details, re.I)
     if m:
-        p['isFoul'] = True
-        p['isInboundFoul'] = True
+        p['is_foul'] = True
+        p['is_inbound_foul'] = True
         p.update(m.groupdict())
         p['team'] = aw if is_hm else hm
         p['opp'] = hm if is_hm else aw
-        p['foulTeam'] = p['opp']
+        p['foul_team'] = p['opp']
         return p
 
     # parsing flagrant fouls
-    flagrantRE = (r'Flagrant foul type (?P<flagType>1|2) by (?P<fouler>{0})'
-                  r'(?: \(drawn by (?P<drewFoul>{0})\))?').format(playerRE)
+    flagrantRE = (r'Flagrant foul type (?P<flag_type>1|2) by (?P<fouler>{0})'
+                  r'(?: \(drawn by (?P<drew_foul>{0})\))?').format(player_re)
     m = re.match(flagrantRE, details, re.I)
     if m:
-        p['isFoul'] = True
-        p['isFlagrant'] = True
+        p['is_foul'] = True
+        p['is_flagrant'] = True
         p.update(m.groupdict())
-        p['foulTeam'] = hm if is_hm else aw
+        p['foul_team'] = hm if is_hm else aw
         return p
 
     # parsing clear path fouls
     clearPathRE = (r'Clear path foul by (?P<fouler>{0})'
-                   r'(?: \(drawn by (?P<drewFoul>{0})\))?').format(playerRE)
+                   r'(?: \(drawn by (?P<drew_foul>{0})\))?').format(player_re)
     m = re.match(clearPathRE, details, re.I)
     if m:
-        p['isFoul'] = True
-        p['isClearPathFoul'] = True
+        p['is_foul'] = True
+        p['is_clear_path_foul'] = True
         p.update(m.groupdict())
         p['team'] = aw if is_hm else hm
         p['opp'] = hm if is_hm else aw
-        p['foulTeam'] = p['opp']
+        p['foul_team'] = p['opp']
         return p
 
     # parsing timeouts
-    timeoutRE = r'(?P<timeoutTeam>.*?) (?:full )?timeout'
+    timeoutRE = r'(?P<timeout_team>.*?) (?:full )?timeout'
     m = re.match(timeoutRE, details, re.I)
     if m:
-        p['isTimeout'] = True
+        p['is_timeout'] = True
         p.update(m.groupdict())
         p['team'] = hm if is_hm else aw
         p['opp'] = aw if is_hm else hm
-        isOfficialTO = p['timeoutTeam'] == 'Official'
-        p['timeoutTeam'] = ('Official' if isOfficialTO else
-                            sportsref.nba.Season(yr).team_names_to_ids()
-                            .get(p['team'], p['team']))
+        isOfficialTO = p['timeout_team'].lower() == 'official'
+        p['timeout_team'] = ('Official' if isOfficialTO else
+                             sportsref.nba.Season(yr).team_names_to_ids()
+                             .get(p['team'], p['team']))
         return p
 
     # parsing technical fouls
-    techRE = r'Technical foul by (?P<fouler>{}|Team)'.format(playerRE)
+    techRE = (r'(?P<is_hanging>Hanging )?'
+              r'(?P<is_taunting>Taunting )?'
+              r'(?P<is_ill_def>Ill def )?'
+              r'(?P<is_delay>Delay )?'
+              r'(?P<is_unsport>Non unsport )?'
+              r'tech(?:nical)? foul by '
+              r'(?P<fouler>{0}|Team)').format(player_re)
     m = re.match(techRE, details, re.I)
     if m:
-        p['isFoul'] = True
-        p['isTechFoul'] = True
+        p['is_tech_foul'] = True
         p.update(m.groupdict())
-        p['foulTeam'] = hm if is_hm else aw
+        p['is_hanging'] = bool(p['is_hanging'])
+        p['is_taunting'] = bool(p['is_taunting'])
+        p['is_ill_def'] = bool(p['is_ill_def'])
+        p['is_delay'] = bool(p['is_delay'])
+        p['is_unsport'] = bool(p['is_unsport'])
+        p['foul_team'] = hm if is_hm else aw
         return p
 
     # parsing ejections
-    ejectRE = r'(?P<ejectee>{0}) ejected from game'.format(playerRE)
+    ejectRE = r'(?P<ejectee>{0}) ejected from game'.format(player_re)
     m = re.match(ejectRE, details, re.I)
     if m:
-        p['isEjection'] = True
-        p['ejecteeTeam'] = hm if is_hm else aw
+        p['is_ejection'] = True
+        p['ejectee_team'] = hm if is_hm else aw
         return p
 
     # parsing defensive 3 seconds techs
     def3TechRE = (r'(?:Def 3 sec tech foul|Defensive three seconds)'
-                  r' by (?P<fouler>{})').format(playerRE)
+                  r' by (?P<fouler>{})').format(player_re)
     m = re.match(def3TechRE, details, re.I)
     if m:
-        p['isFoul'] = True
-        p['isTechFoul'] = True
-        p['isDefThreeSecs'] = True
+        p['is_tech_foul'] = True
+        p['is_def_three_secs'] = True
         p.update(m.groupdict())
-        p['foulTeam'] = hm if is_hm else aw
+        p['foul_team'] = hm if is_hm else aw
         return p
 
     # parsing violations
     violRE = (r'Violation by (?P<violator>{0}|Team) '
-              r'\((?P<violType>.*)\)').format(playerRE)
+              r'\((?P<viol_type>.*)\)').format(player_re)
     m = re.match(violRE, details, re.I)
     if m:
-        p['isViolation'] = True
+        p['is_viol'] = True
         p.update(m.groupdict())
-        p['violTeam'] = hm if is_hm else aw
+        p['viol_team'] = hm if is_hm else aw
         return p
 
-    p['isError'] = True
+    p['is_error'] = True
     return p
 
 
@@ -299,7 +328,7 @@ def clean_features(df):
     """Fixes up columns of the passed DataFrame, such as casting T/F columns to
     boolean and filling in NaNs for team and opp.
 
-    :df: DataFrame of play-by-play data.
+    :param df: DataFrame of play-by-play data.
     :returns: Dataframe with cleaned columns.
     """
     df = copy.deepcopy(df)
