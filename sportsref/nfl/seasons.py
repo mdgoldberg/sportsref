5a29a334f405010c1b6bc7bde58e5d58680c08b3
Phil Lynch <phillynch7@gmail.com>
2017-10-01 00:00:07 -0400

    Add nfl.Season.get_draft_info() to scrape draft picks pages.

--- sportsref/nfl/seasons.py
+++ sportsref/nfl/seasons.py
@@ -19,27 +19,27 @@ class Season(future.utils.with_metaclass(sportsref.decorators.Cached, object)):
 
         :year: The year of the season we want.
         """
-        self.yr = int(year)
+        self.year = int(year)
 
     def __eq__(self, other):
-        return (self.yr == other.yr)
+        return (self.year == other.year)
 
     def __hash__(self):
-        return hash(self.yr)
+        return hash(self.year)
 
     def __repr__(self):
-        return 'Season({})'.format(self.yr)
+        return 'Season({})'.format(self.year)
 
     def _subpage_url(self, page):
         return (sportsref.nfl.BASE_URL +
-                '/years/{}/{}.htm'.format(self.yr, page))
+                '/years/{}/{}.htm'.format(self.year, page))
 
     @sportsref.decorators.memoize
     def get_main_doc(self):
         """Returns PyQuery object for the main season URL.
         :returns: PyQuery object.
         """
-        url = sportsref.nfl.BASE_URL + '/years/{}/'.format(self.yr)
+        url = sportsref.nfl.BASE_URL + '/years/{}/'.format(self.year)
         return pq(sportsref.utils.get_html(url))
 
     @sportsref.decorators.memoize
@@ -56,7 +56,7 @@ class Season(future.utils.with_metaclass(sportsref.decorators.Cached, object)):
         """Returns a list of the team IDs for the given year.
         :returns: List of team IDs.
         """
-        return sportsref.nfl.teams.list_teams(self.yr)
+        return sportsref.nfl.teams.list_teams(self.year)
 
     @sportsref.decorators.memoize
     def team_ids_to_names(self):
@@ -65,14 +65,31 @@ class Season(future.utils.with_metaclass(sportsref.decorators.Cached, object)):
         :returns: Dictionary with team IDs as keys and full team strings as
             values.
         """
-        return sportsref.nfl.teams.team_names(self.yr)
+        return sportsref.nfl.teams.team_names(self.year)
 
     @sportsref.decorators.memoize
     def team_names_to_ids(self):
         """Mapping from full team names to 3-letter team IDs.
         :returns: Dictionary with tean names as keys and team IDs as values.
         """
-        return sportsref.nfl.teams.team_ids(self.yr)
+        return sportsref.nfl.teams.team_ids(self.year)
+
+    @sportsref.decorators.memoize
+    def get_draft_info(self):
+        """Returns a dataframe with draft info from the season.
+        """
+        url = (sportsref.nfl.BASE_URL + '/years/{}/draft.htm'.format(self.year))
+        doc = pq(sportsref.utils.get_html(url))
+        table = doc('table#drafts')
+        df = sportsref.utils.parse_table(table)
+        df['season'] = self.year
+        df['season'] = df['season'].astype(int)
+        cols = ['season','draft_round','draft_pick','team','player_id',
+                'player_name','college_id','position','age']
+        for col in cols:
+            if col not in df: df[col] = np.nan
+        df = df[cols]
+        return df
 
     @sportsref.decorators.memoize
     def _get_player_stats_table(self, subpage, table_id):
