6facc766ad4b2b14169d0eca4a537c838ce5ee59
Alex Beard <abeard1@uvm.edu>
2018-01-18 13:14:06 -0500

    url fix #2

--- sportsref/euro/__init__.py
+++ sportsref/euro/__init__.py
@@ -8,7 +8,7 @@ from .seasons import Season
 from .teams import Team
 from .players import Player
 
-BASE_URL = 'http://www.sports-reference.com/euro'
+BASE_URL = 'http://www.basketball-reference.com/euro'
 
 __all__ = [
     'BASE_URL',
