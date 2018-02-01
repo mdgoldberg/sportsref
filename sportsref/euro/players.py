02f4d8594e0e1d24e71f7cf3323464d12cb8f68a
Alex Beard <abeard1@uvm.edu>
2018-02-01 02:58:51 -0500

    I'm dumb

--- sportsref/euro/players.py
+++ sportsref/euro/players.py
@@ -137,10 +137,10 @@ class Player(future.utils.with_metaclass(sportsref.decorators.Cached, object)):
         :level: specifies Club play (C), Eurocup/Euroleague play (E), or Both (B) - defaults to B
         :returns: A DataFrame of stats.
         """
-        doc = self.get_main_doc(level=level)    
+        doc = self.get_main_doc(level=level)
         if level == 'E': 
             table_id = 'table#{}EUR0'.format(table_id)
-        if level =='C':
+        elif level =='C':
             table_id = 'table#{}{}'.format(table_id, 'CLU1' if kind == 'P' else 'CLU0')   
         else:
             table_id = 'table#{}{}'.format(table_id, 'ALL1' if kind == 'P' else 'ALL0')
