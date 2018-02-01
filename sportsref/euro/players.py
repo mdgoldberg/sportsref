822c989e736931922e4ebcf88d7fcca7e220dd19
Alex Beard <abeard1@uvm.edu>
2018-01-31 20:30:51 -0500

    added level variable for All/Euro/Club play (very different than NBA)

--- sportsref/euro/players.py
+++ sportsref/euro/players.py
@@ -17,15 +17,16 @@ __all__ = [
 
 class Player(future.utils.with_metaclass(sportsref.decorators.Cached, object)):
 
-    """Each instance of this class represents an CBB player, uniquely
+    """Each instance of this class represents an EuroBB player, uniquely
     identified by a player ID. The instance methods give various data available
     from the player's Sports Reference player page."""
 
     def __init__(self, player_id):
         self.player_id = player_id
-        self.url_base = (sportsref.euro.BASE_URL +
-                         '/players/{0}').format(self.player_id)
-        self.main_url = self.url_base + '.htm'
+        self.main_url = self.get_main_url(player_id)
+
+    def get_main_url(self, player_id):
+        return (sportsref.euro.BASE_URL + '/players/{0}.htm').format(player_id)
 
     def __eq__(self, other):
         return self.player_id == other.player_id
@@ -40,18 +41,20 @@ class Player(future.utils.with_metaclass(sportsref.decorators.Cached, object)):
         return self.name()
 
     @sportsref.decorators.memoize
-    def get_main_doc(self):
-        return pq(sportsref.utils.get_html(self.main_url))
+    def get_main_doc(self, level='B'):
+       #Need to have a level option this deep in process because all vs club vs tourney means a completely    different doc for euro, instead of nba which is just a different table and same doc
 
+        player_id = self.player_id
+        if level == 'C':
+            player_id += '-club'
+        if level == 'E':
+            player_id += '-tournament'
+        return pq(sportsref.utils.get_html(self.get_main_url(player_id))) 
+    
     @sportsref.decorators.memoize
     def get_gamelog_doc(self, year, league):
         url = '{}/pgl_euro.cgi?player_id={}&year_id={}&lg_id={}'.format(sportsref.euro.BASE_URL, self.player_id, year, league)
         return pq(sportsref.utils.get_html(url))  
-
-    @sportsref.decorators.memoize
-    def get_sub_doc(self, rel_url):
-        url = '{}/{}'.format(self.url_base, rel_url)
-        return pq(sportsref.utils.get_html(url))
 
     @sportsref.decorators.memoize
     def available_gamelogs(self):
@@ -124,37 +127,41 @@ class Player(future.utils.with_metaclass(sportsref.decorators.Cached, object)):
             return None
 
     @sportsref.decorators.kind_rpb(include_type=True)
-    def _get_stats_table(self, table_id, kind='R', summary=False):
+    def _get_stats_table(self, table_id, kind='R', level='B', summary=False):
         """Gets a stats table from the player page; helper function that does
         the work for per-game, per-36-min, etc. stats.
 
         :table_id: the ID of the HTML table.
         :kind: specifies regular season, playoffs. One of 'R', 'P'.
           Defaults to 'R'.
+        :level: specifies Club play (C), Eurocup/Euroleague play (E), or Both (B) - defaults to B
         :returns: A DataFrame of stats.
         """
-        doc = self.get_main_doc()
-        table_id = 'table#{}{}'.format(
-            table_id, 'ALL1' if kind == 'P' else 'ALL0')
+        doc = self.get_main_doc(level=level)
+        if level == 'E':
+            table_id = 'table#{}EUR0'.format(table_id)
+        else:
+            table_id = 'table#{}{}'.format(table_id, 'ALL1' if kind == 'P' else 'ALL0')
 
+        print(table_id)
         table = doc(table_id)
         df = sportsref.utils.parse_table(table, flatten=(not summary), footer=summary)
         return df
 
     @sportsref.decorators.memoize
-    def stats_per_game(self, kind='R', summary=False):
+    def stats_per_game(self, kind='R', level='B', summary=False):
         """Returns a DataFrame of per-game box score stats."""
-        return self._get_stats_table('per_game', kind=kind, summary=summary)
+        return self._get_stats_table('per_game', kind=kind, level=level, summary=summary)
 
     @sportsref.decorators.memoize
-    def stats_totals(self, kind='R', summary=False):
+    def stats_totals(self, kind='R', level='B', summary=False):
         """Returns a DataFrame of total box score statistics by season."""
-        return self._get_stats_table('totals', kind=kind, summary=summary)
+        return self._get_stats_table('totals', kind=kind, level=level, summary=summary)
 
     @sportsref.decorators.memoize
-    def stats_per36(self, kind='R', summary=False):
+    def stats_per36(self, kind='R', level='B', summary=False):
         """Returns a DataFrame of per-36-minutes stats."""
-        return self._get_stats_table('per_minute', kind=kind, summary=summary)
+        return self._get_stats_table('per_minute', kind=kind, level=level, summary=summary)
 
     @sportsref.decorators.memoize
     def stats_advanced(self, kind='R', summary=False):
