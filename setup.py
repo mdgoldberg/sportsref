7b276be35b79ebc456babacb86f9433eef701404
Matt Goldberg <matt.goldberg7@gmail.com>
2018-02-03 12:09:39 -0800

    updated my email

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(name='sportsref',
       description='Scraping data from sports-reference.com and related sites',
       url='https://github.com/mdgoldberg/sportsref',
       author='Matt Goldberg',
-      author_email='mgoldberg@college.harvard.edu',
+      author_email='matt.goldberg7@gmail.com',
       packages=find_packages(),
       install_requires=[
           'appdirs',
